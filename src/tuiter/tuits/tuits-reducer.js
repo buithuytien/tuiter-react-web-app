d5b9d88bad3c84224e651ca8642d0ec298506dc1
buithuytien <buithuytien13.13@gmail.com>
2022-11-18 15:09:00 -0500

    a8 start tuitsthunk

--- src/tuiter/tuits/tuits-reducer.js
+++ src/tuiter/tuits/tuits-reducer.js
@@ -1,6 +1,14 @@
 import { createSlice } from "@reduxjs/toolkit";
 import tuits from './tuits.json';
 
+import {findTuitsThunk} from "../../services/tuits-thunks";
+
+const initialState = {
+    tuits: [],
+    loading: false
+}
+
+
 const currentUser = {
     "userName": "NASA",
     "handle": "@nasa",
@@ -20,6 +28,23 @@ const templateTuit = {
 const tuitsSlice = createSlice({
     name: 'tuits',
     initialState: tuits,
+    extraReducers: {
+        [findTuitsThunk.pending]:
+            (state) => {
+                state.loading = true
+                state.tuits = []
+            },
+        [findTuitsThunk.fulfilled]:
+            (state, { payload }) => {
+                state.loading = false
+                state.tuits = payload
+            },
+        [findTuitsThunk.rejected]:
+            (state) => {
+                state.loading = false
+            }
+    },
+
     reducers: {   deleteTuit(state, action) {
             const index = state
                 .findIndex(tuit =>

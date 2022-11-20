2af61920a3307419703045b80934942dd896bc89
buithuytien <buithuytien13.13@gmail.com>
2022-11-20 14:54:02 -0500

    a8 add ...templateTuit what's happenning line 15 remove old reducers

--- src/tuiter/tuits/tuits-reducer.js
+++ src/tuiter/tuits/tuits-reducer.js
@@ -73,34 +73,34 @@ const tuitsSlice = createSlice({
             }
     },
 
-    reducers: {   deleteTuit(state, action) {
-            const index = state
-                .findIndex(tuit =>
-                    tuit._id === action.payload);
-            state.splice(index, 1);
-        },
-        createTuit(state, action) {
-            state.unshift({
-                ...action.payload,
-                ...templateTuit,
-                _id: (new Date()).getTime(),
-            })
-        },
-        likeTuit(state, action) {
-            const tuit = state.
-            find((tuit) =>
-                tuit._id === action.payload);
-            if (tuit!=null) {
-                tuit.liked = !tuit.liked;
-                if (tuit.liked) {
-                    tuit.likes++;
-                } else {
-                    tuit.likes--;
-                }
-            }
-        }
-    }
+    // reducers: {   deleteTuit(state, action) {
+    //         const index = state
+    //             .findIndex(tuit =>
+    //                 tuit._id === action.payload);
+    //         state.splice(index, 1);
+    //     },
+    //     createTuit(state, action) {
+    //         state.unshift({
+    //             ...action.payload,
+    //             ...templateTuit,
+    //             _id: (new Date()).getTime(),
+    //         })
+    //     },
+    //     likeTuit(state, action) {
+    //         const tuit = state.
+    //         find((tuit) =>
+    //             tuit._id === action.payload);
+    //         if (tuit!=null) {
+    //             tuit.liked = !tuit.liked;
+    //             if (tuit.liked) {
+    //                 tuit.likes++;
+    //             } else {
+    //                 tuit.likes--;
+    //             }
+    //         }
+    //     }
+    // }
 });
 
-export const {createTuit, deleteTuit, likeTuit} = tuitsSlice.actions;
+// export const {createTuit, deleteTuit, likeTuit} = tuitsSlice.actions;
 export default tuitsSlice.reducer;

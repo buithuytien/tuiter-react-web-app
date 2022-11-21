dcc61c81b9fa2742997ce1955e9f84850691ed6f
buithuytien <buithuytien13.13@gmail.com>
2022-11-20 20:56:38 -0500

    a8 tuit-stat-item col flexbox onclick likehandler

--- src/tuiter/tuits/tuit-stats/tuit-stat-item.js
+++ src/tuiter/tuits/tuit-stats/tuit-stat-item.js
@@ -9,17 +9,8 @@ const TuitStatItem = ({post}) => {
         // dispatch(likeTuit(id));
         dispatch(updateTuitThunk({
             ...post,
-            likes: post.likes + 1,
-            liked: true
-        }))
-    };
-
-    const unlikeToggleHandler = (id) => {
-        // dispatch(likeTuit(id));
-        dispatch(updateTuitThunk({
-            ...post,
-            likes: Math.max(post.likes - 1, ),
-            liked: false
+            likes: post.liked? post.likes - 1 : post.likes + 1,
+            liked: !post.liked
         }))
     };
 
@@ -27,55 +18,50 @@ const TuitStatItem = ({post}) => {
         // dispatch(likeTuit(id));
         dispatch(updateTuitThunk({
             ...post,
-            dislikes: post.dislikes + 1,
-            disliked: true
-        }))
-    };
-
-    const undislikeToggleHandler = (id) => {
-        // dispatch(likeTuit(id));
-        dispatch(updateTuitThunk({
-            ...post,
-            dislikes: Math.max(post.dislikes - 1),
-            disliked: false
+            dislikes: post.disliked? post.dislikes - 1 : post.dislikes + 1,
+            disliked: !post.disliked
         }))
     };
 
     return (
         <div className="row border-0 mt-2">
-                <span className="col-2">
+                <span className="col">
                     <i className="ps-4 fa fa-comment-o"> </i>
                     <span className="ps-2">{post.replies}</span>
                 </span>
-            <span className="col-2">
+            <span className="col">
                     <span className="ps-4 fa fa-retweet"> </span>
                     <span className="ps-2">{post.retuits}</span>
                 </span>
-            <span className="col-3">
+            <span className="col">
                 <span className= "ps-4">
-                   {/*<span className={`ps-2 pe-1 small ${post.liked? 'text-danger fa fa-heart' :'fa fa-heart-o'}`}*/}
-                   {/*      onClick={() => {likeToggleHandler(post._id)}}></span>*/}
-                   {/*     {post.likes}*/}
+                   <span className={`ps-2 pe-1 small ${post.liked? 'text-danger fa fa-heart' :'fa fa-heart-o'}`}
+                         onClick={() => {likeToggleHandler(post._id)}}>
+                   </span>
+                        {post.likes}
 
-                    {post.liked && <i onClick={() => {unlikeToggleHandler(post._id)}} className="ps-2 pe-1 small text-danger fa fa-heart"></i>}
-                    {!post.liked && <i onClick={() => {likeToggleHandler(post._id)}} className="ps-2 pe-1 small fa fa-heart-o"></i>}
-                    {post.likes}
                     </span>
 
                 </span>
 
 
-            <span className="col-3">
+            <span className="col">
                 <span className= "ps-4">
 
-                    {post.disliked && <i onClick={() => {undislikeToggleHandler(post._id)}} className="ps-2 pe-1 small text-danger fa fa-thumbs-down"></i>}
-                    {!post.disliked && <i onClick={() => {dislikeToggleHandler(post._id)}} className="ps-2 pe-1 small fa fa-thumbs-down"></i>}
+                    {/*{post.disliked && <i onClick={() => {undislikeToggleHandler(post._id)}} className="ps-2 pe-1 small text-danger fa fa-thumbs-down"></i>}*/}
+                    {/*{!post.disliked && <i onClick={() => {dislikeToggleHandler(post._id)}} className="ps-2 pe-1 small fa fa-thumbs-down"></i>}*/}
+                    {/*{post.dislikes}*/}
+                    <span className={`ps-2 pe-1 small ${post.disliked? 'text-danger fa fa-thumbs-down"' :'fa fa-thumbs-down"'}`}
+                          onClick={() => {dislikeToggleHandler(post._id)}}>
+                   </span>
                     {post.dislikes}
+
+
                     </span>
 
                 </span>
 
-            <span className="col-2">
+            <span className="col">
                     <span className="ps-4 fa fa-share-alt"></span>
                 </span>
         </div>

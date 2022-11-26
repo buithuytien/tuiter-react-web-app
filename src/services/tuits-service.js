40cb9817f6d913c069666c12362256c0c6ffa258
buithuytien <buithuytien13.13@gmail.com>
2022-11-26 15:13:38 -0500

    a9 replace tuit_api string in .zshenv

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -3,7 +3,6 @@ import axios from 'axios';
 const API_BASE = process.env.REACT_APP_API_BASE;
 console.log(API_BASE)
 const TUITS_API = `${API_BASE}/tuits`;
-console.log(API_BASE)
 
 // const TUITS_API = 'http://localhost:4000/api/tuits';
 
@@ -21,8 +20,7 @@ export const findTuits = async () => {
 }
 
 export const deleteTuit = async (tid) => {
-    const response = await axios
-        .delete(`${TUITS_API}/${tid}`)
+    const response = await axios.delete(`${TUITS_API}/${tid}`)
     return response.data
 }
 
